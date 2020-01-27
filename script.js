0a3f61fbeac8ab4eebcb561e348d9bb6ed8c3f10
Yogasmara <septianayoga30@gmail.com>
2020-01-27 08:31:50 +0700

    add storing value

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// Script goes here
+// getting tag from html
+let displayScreen = document.querySelector('.result-screen');
+let calcButton = document.querySelectorAll('button');
+let displayValue = "";
+
+// Basic math function
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+// operate function
+function operate(operator, number1, number2) {
+
+}
+
+// display function
+function populateDisplay(e) {
+  displayScreen.insertAdjacentHTML('beforeend', e.target.dataset.button);
+  displayValue += e.target.dataset.button;
+  console.log(displayValue);
+}
+
+// button event listener
+calcButton.forEach(key => key.addEventListener('click', populateDisplay));
