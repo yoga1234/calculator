8783cf564aa8c4ccffd05d3ebd8472fb2ceeb4d1
Yogasmara <septianayoga30@gmail.com>
2020-01-27 21:06:26 +0700

    add some error

--- script.js
+++ script.js
@@ -39,31 +39,59 @@ function operate(operator, number1, number2) {
   }
 }
 
-// display function
+// updating the display
+function displayFunction(valueTaken, operatorTaken) {
+  // check for the operation
+  if(operatorTaken == "number") {
+    // update the display
+    displayScreen.insertAdjacentHTML('beforeend', `<span class="result-value">${valueTaken}</span>`);
+    // add valueTaken into displayValue
+    displayValue += valueTaken;
+  } else if(operatorTaken == "operator") {
+    // if user click operator
+    displayScreen.insertAdjacentHTML('beforeend', `<span class="result-value">${valueTaken}</span>`);
+    firstNumber = displayValue;
+    console.log(firstNumber);
+    operatorNumber = valueTaken;
+    displayValue = '';
+  } else if(operatorTaken == "clear"){
+    clearDisplay();
+  } else if(operatorTaken == "delete"){
+    deleteDisplay();
+  } else {
+    // if user click equal sign
+    secondNumber = displayValue;
+    result = operate(operatorNumber, firstNumber, secondNumber);
+    displayScreen.innerHTML = '';
+    displayValue = result;
+    displayScreen.insertAdjacentHTML('beforeend', result);
+    historyScreen.innerHTML = '';
+    historyScreen.insertAdjacentHTML('beforeend', firstNumber + operatorNumber + secondNumber);
+  }
+}
+
+// populating display
 function populateDisplay(e) {
   let valueTaken = e.target.dataset.button.split(',')[0].trim();
   let operatorTaken = e.target.dataset.button.split(',')[1].trim();
 
-  // check for the operation
-  if(operatorTaken == "number") {
-    displayScreen.insertAdjacentHTML('beforeend', valueTaken);
-    historyScreen.insertAdjacentHTML('beforeend', valueTaken);
-    displayValue += valueTaken;
-  } else if(operatorTaken == "operator") {
-    displayScreen.insertAdjacentHTML('beforeend', valueTaken);
-    historyScreen.insertAdjacentHTML('beforeend', valueTaken);
-    firstNumber = displayValue;
-    operatorNumber = valueTaken;
-    displayValue = '';
-  } else {
-    secondNumber = displayValue;
-    displayValue = '';
-    result = operate(operatorNumber, firstNumber, secondNumber);
-    displayScreen.innerHTML = '';
-    displayScreen.insertAdjacentHTML('beforeend', result);
-  }
+  displayFunction(valueTaken, operatorTaken);
+}
 
-  // console.log(e.target.dataset.button.split(',')[1].trim());
+// clear display function
+function clearDisplay() {
+  historyScreen.innerHTML = '';
+  displayScreen.innerHTML = '';
+}
+
+// delete display function
+function deleteDisplay() {
+  let resultValue = document.querySelectorAll('.result-value');
+  if(!resultValue.length) {
+    return;
+  } else {
+    displayScreen.removeChild(displayScreen.lastChild);
+  }
 }
 
 // button event listener
