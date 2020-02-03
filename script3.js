f1d9e93b270930dc66815cc590a3baf8c73ff8c7
Yogasmara <septianayoga30@gmail.com>
2020-02-03 20:47:07 +0700

    update failure

--- script3.js
+++ script3.js
@@ -4,20 +4,40 @@ const historyDisplay = document.querySelector('.history-screen');
 const display = document.querySelector('.display');
 // get all of the button
 const calcButton = document.querySelectorAll('button');
+// container for saving custom attribute
+const container = document.querySelector('.container');
 
-// main calculate function
-function calculate(e) {
+// function for calculating the result
+const calculate = (n1, operator, n2) => {
+  let result = '';
+
+  if(operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if(operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if(operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if(operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+
+  return result;
+}
+
+// main calculator function
+function mainFunc(e) {
   const key = e.target;
   const action = key.dataset.button;
   const keyContent = key.textContent; // get the number with textContent
   const displayedNum = display.textContent; // get the display with textContent
+  const previousKeyType = container.dataset.previousKeyType;
 
   // remove .key-pressed class from all keys
   Array.from(key.parentNode.children).forEach(k => k.classList.remove('key-pressed'));
   
   // user click on the number key
   if(action === 'number') {
-    if(displayedNum === '0') {
+    if(displayedNum === '0' || previousKeyType === 'operator') {
       display.textContent = keyContent;
     } else {
       display.textContent = displayedNum + keyContent;
@@ -32,6 +52,11 @@ function calculate(e) {
     action === 'divide'
   ) {
     key.classList.add('key-pressed');
+    // add custom attribute
+    container.dataset.previousKeyType = 'operator';
+    // saving the calculation value
+    container.dataset.firstValue = displayedNum;
+    container.dataset.operator = action;
   }
 
   // user click on the decimal key
@@ -46,7 +71,11 @@ function calculate(e) {
 
   // user click on the equal key
   if(action === 'equal') {
-    console.log("you are pressed on the equal key");
+    const firstValue = container.dataset.firstValue;
+    const operator = container.dataset.operator;
+    const secondValue = displayedNum;
+
+    display.textContent = calculate(firstValue, operator, secondValue);
   }
 
   // user click on the delete button
@@ -57,4 +86,4 @@ function calculate(e) {
 }
 
 // add event listener to all of the button
-calcButton.forEach(key => key.addEventListener('click', calculate));
+calcButton.forEach(key => key.addEventListener('click', mainFunc));
