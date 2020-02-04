3e204e904c2a1110f468d0bad2b0f91eeda067c8
Yogasmara <septianayoga30@gmail.com>
2020-02-04 12:05:19 +0700

    deleting trash file

--- backupcode.js
+++
@@ -1,8 +0,0 @@
-// this from consecutive calculation
-  secondNumber = parseInt(resultDisplay.innerHTML); 
-  operatorNumber = valueTaken;// saving the first number
-  historyDisplay.innerHTML += secondNumber;
-  historyDisplay.innerHTML += operatorNumber;
-  firstNumber = calculation(firstNumber, secondNumber, operatorNumber);      
-  console.log(firstNumber);
-  resultDisplay.innerHTML = '';
